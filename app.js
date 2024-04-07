0122180a794e5f692a6d58e7d12d3977db129f63
Priscilla Rojas <73943767+Priscilla-Rojas@users.noreply.github.com>
2024-04-07 17:12:16 -0300

    resposive del slaider del home, e incio del funcionamiento del nav del header en vercion movil

---
+++ app.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.header-nav');
+const header = document.querySelector('.header');
+const close = document.querySelector('.close');
+
+hamburger.addEventListener('click', ()=>{
+  nav.classList.toggle('visible');
+  header.classList.toggle('header-visible');
+  console.log(nav)
+})
+close.addEventListener('click', ()=>{
+  nav.classList.toggle('visible');
+  header.classList.toggle('header-visible');
+})
